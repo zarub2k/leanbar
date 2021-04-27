f35a536a27917be2ec1ea6f9e18b780b8d7b7e77
Tham <zarub2k@gmail.com>
2021-04-27 10:49:16 +0530

    URL changed

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const apiClient = axios.create({
-  // baseURL: 'https://devportal.saglive.com/rest/v1',
-  baseURL: "http://localhost:8080/rest/v1",
+  baseURL: 'https://devportal.saglive.com/rest/v1',
+  // baseURL: "http://localhost:8080/rest/v1",
   withCredentials: false,
   headers: {
     Accept: "application/json",
