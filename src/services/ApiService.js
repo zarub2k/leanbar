adf81628a50fb713a1f05bbcf8f5a144c095df1a
Tham <zarub2k@gmail.com>
2021-07-27 11:10:19 +0530

    path changed

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const apiClient = axios.create({
   // baseURL: 'https://devportal.saglive.com/rest/v1',
-  baseUrl: 'https://trialdemoaws.apiportal-aw-us.webmethods-int.io/rest/v1',
   // baseURL: "http://localhost:8080/rest/v1",
+  baseURL: 'https://trialdemoaws.apiportal-aw-us.webmethods-int.io/rest/v1',
   withCredentials: false,
   headers: {
     Accept: "application/json",
